f6c2b101fd81be8644ec0d63326afc384a19d403
hedgehog319 <fign1910@mail.ru>
2022-05-12 19:35:43 +1000

    HW3

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,18 @@
 const mix = require('laravel-mix');
+const { resolve } = require('path');
 
 mix
-    .setPublicPath('public')
     .js('resources/scripts/main.js', 'build/bundle.js')
-    .sass('resources/styles/main.scss', 'build/bundle.css')
+    .sass('resources/styles/main.scss', 'build/bundle.css', {}, [
+        require('postcss-axis')
+    ])
     .browserSync({
         proxy: 'localhost',
         files: [
             'public/**/*',
             'resources/**/*.twig',
-            'resources/**/*.php',
-        ],
+            'resources/**/*.php'
+        ]
     });
+
+mix.alias({'uikit-util': resolve(__dirname, 'node_modules/uikit/src/js/util')});
